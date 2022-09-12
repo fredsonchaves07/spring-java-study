8f400cee8a8afecb6cfc065ed21895034aee03a7
fredsonchaves07 <fredsonchaves07@gmail.com>
2022-09-12 09:57:28 -0300

    populando banco de dados e configurando testes
    Signed-off-by: fredsonchaves07 <fredsonchaves07@gmail.com>

--- src/test/java/com/fredsonchaves/algafood/CadastroCozinhaIntegrationIT.java
+++ src/test/java/com/fredsonchaves/algafood/CadastroCozinhaIntegrationIT.java
@@ -1,26 +1,39 @@
 package com.fredsonchaves.algafood;
 
+import com.fredsonchaves.algafood.domain.entity.Cozinha;
+import com.fredsonchaves.algafood.domain.repository.CozinhaRepository;
+import com.fredsonchaves.algafood.utils.DatabaseCleaner;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 
 import static io.restassured.RestAssured.given;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("application-test.properties")
 class CadastroCozinhaIntegrationIT {
 
     @LocalServerPort
     private int port;
+
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
 
     @BeforeEach
     public void setUp() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.port = port;
         RestAssured.basePath = "/cozinhas";
+        databaseCleaner.clearTables();
     }
 
     @Test
@@ -31,5 +44,14 @@ class CadastroCozinhaIntegrationIT {
                 .accept(ContentType.JSON)
                 .when().get()
                 .then().statusCode(HttpStatus.OK.value());
+    }
+
+    private void prepararDados() {
+        Cozinha cozinha1 = new Cozinha();
+        cozinha1.setNome("Tailandesa");
+        cozinhaRepository.save(cozinha1);
+        Cozinha cozinha2 = new Cozinha();
+        cozinha2.setNome("Brasileira");
+        cozinhaRepository.save(cozinha2);
     }
 }
