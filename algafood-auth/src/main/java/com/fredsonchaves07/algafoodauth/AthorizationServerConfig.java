f23d2e4a8ea0d86da5ef490bea31fe9251af1372
fredsonchaves07 <fredsonchaves07@gmail.com>
2022-12-20 12:03:55 -0300

    feat: implementacao do authorization resource server
    Signed-off-by: fredsonchaves07 <fredsonchaves07@gmail.com>

--- algafood-auth/src/main/java/com/fredsonchaves07/algafoodauth/AthorizationServerConfig.java
+++ algafood-auth/src/main/java/com/fredsonchaves07/algafoodauth/AthorizationServerConfig.java
@@ -1,7 +1,9 @@
 package com.fredsonchaves07.algafoodauth;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @Configuration
+@EnableAuthorizationServer
 public class AthorizationServerConfig {
 }
