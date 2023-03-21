d9fa4d43bf699b3cb0b1cb5ce3857d22f5af0ddb
Fredson <fredsonchaves07@gmail.com>
2023-03-21 11:19:08 -0300

    feat: remove project

--- grpc-product-example/src/main/java/com/fredsonchaves07/grpcproductexample/domain/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fredsonchaves07.grpcproductexample.domain.repository;
-
-import com.fredsonchaves07.grpcproductexample.domain.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, String> {
-}
--- grpc-product-example/src/main/java/com/fredsonchaves07/grpcproductexample/domain/service/IProductService.java
+++
@@ -1,8 +0,0 @@
-package com.fredsonchaves07.grpcproductexample.domain.service;
-
-import com.fredsonchaves07.grpcproductexample.domain.dto.ProductDTO;
-
-public interface IProductService {
-    String create(ProductDTO inputDTO);
-    ProductDTO findById(String productId);
-}
